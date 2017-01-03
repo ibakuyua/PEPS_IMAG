e69063c2ffba5411bed187aea3d75593ab7b1403
Arnaud Ibakuyumcu <arnaud.ibakuyumcu@ensimag.grenoble-inp.fr>
2017-01-03 13:56:22 +0100

    [AJOUT DES INFOS INDEX] Prix 92 ok

--- src/Pricer/Multimonde.hpp
+++ src/Pricer/Multimonde.hpp
@@ -1,23 +1,7 @@
 #ifndef PRICING_MULTIMONDE_HPP
 #define PRICING_MULTIMONDE_HPP
 
-// Number of days from 01/10/2015 to each date of constatation
-#define NB_DAYS_TO_CONSTATATION_1 373 // 01/10/2015 to 07/10/2016
-#define NB_DAYS_TO_CONSTATATION_2 744 // 01/10/2015 to 13/10/2017
-#define NB_DAYS_TO_CONSTATATION_3 1115 // 01/10/2015 to 19/10/2018
-#define NB_DAYS_TO_CONSTATATION_4 1486 // 01/10/2015 to 25/10/2019
-#define NB_DAYS_TO_CONSTATATION_5 1857 // 01/10/2015 to 30/10/2020
-#define NB_DAYS_TO_CONSTATATION_6 2228 // 01/10/2015 to 05/11/2021
-
-#define NB_DAYS_TO_MATURITY 2235 // 01/10/2015 to 12/11/2021
-#define BASE 360
-
-// Cap and Floor
-#define FLOOR -0.15
-#define CAP 0.15
-
-// Nominal
-#define NOMINAL 100
+#include "infoMultimonde.hpp"
 
 #include "Pricing/PricerGen.hpp"
 
--- src/Pricer/Tests/testMultimonde.cpp
+++ src/Pricer/Tests/testMultimonde.cpp
@@ -23,7 +23,7 @@ int main(){
     ModelGen *simuIndex = new BlackScholesIndexModel(6,spot, trend, volatility, choleskyCorr, constRate);
     ModelGen *simuChange;
     int nbSample = 50000;
-    int nbTimeStep = 9000;
+    int nbTimeStep = 100;
     PricerGen * pricer = new MonteCarloPricer(
             Multimonde::maturity,Multimonde::payOff,simuIndex,simuChange,nbSample, nbTimeStep);
     assert(pricer != NULL);
@@ -49,10 +49,44 @@ int main(){
 }
 
 void setParameters(PnlVect **spot, PnlVect **trend, PnlVect **volatility, PnlMat **choleskyCorr){
-    *spot = pnl_vect_create_from_scalar(6,100);
+    // TODO : Peut être faire des méthodes venant de multimonde
+    *spot = pnl_vect_create(6);
+    PNL_SET(*spot,0,SPOT_FTSE);
+    PNL_SET(*spot,1,SPOT_P500);
+    PNL_SET(*spot,2,SPOT_HANGSENG);
+    PNL_SET(*spot,3,SPOT_NIKKEI);
+    PNL_SET(*spot,4,SPOT_SPASX200);
+    PNL_SET(*spot,5,SPOT_EUROSTOCK50);
     *trend = pnl_vect_create_from_scalar(6,0.07);
-    *volatility = pnl_vect_create_from_scalar(6,0.04);
-    *choleskyCorr = pnl_mat_create_from_scalar(6,6,0.01);
+    *volatility = pnl_vect_create(6);
+    PNL_SET(*volatility,0,VOL_FTSE);
+    PNL_SET(*volatility,1,VOL_P500);
+    PNL_SET(*volatility,2,VOL_HANGSENG);
+    PNL_SET(*volatility,3,VOL_NIKKEI);
+    PNL_SET(*volatility,4,VOL_SPASX200);
+    PNL_SET(*volatility,5,VOL_EUROSTOCK50);
+    *choleskyCorr = pnl_mat_create(6,6);
+    PNL_MSET(*choleskyCorr,0,1,COR_FTSE_P500);
+    PNL_MSET(*choleskyCorr,0,2,COR_FTSE_HANGSENG);
+    PNL_MSET(*choleskyCorr,0,3,COR_FTSE_NIKKEI);
+    PNL_MSET(*choleskyCorr,0,4,COR_FTSE_SPASX200);
+    PNL_MSET(*choleskyCorr,0,5,COR_FTSE_EUROSTOCK50);
+    PNL_MSET(*choleskyCorr,1,2,COR_P500_HANGSENG);
+    PNL_MSET(*choleskyCorr,1,3,COR_P500_NIKKEI);
+    PNL_MSET(*choleskyCorr,1,4,COR_P500_SPASX200);
+    PNL_MSET(*choleskyCorr,1,5,COR_P500_EUROSTOCK50);
+    PNL_MSET(*choleskyCorr,2,3,COR_HANGSENG_NIKKEI);
+    PNL_MSET(*choleskyCorr,2,4,COR_HANGSENG_SPASX200);
+    PNL_MSET(*choleskyCorr,2,5,COR_HANGSENG_EUROSTOCK50);
+    PNL_MSET(*choleskyCorr,3,4,COR_NIKKEI_SPASX200);
+    PNL_MSET(*choleskyCorr,3,5,COR_NIKKEI_EUROSTOCK50);
+    PNL_MSET(*choleskyCorr,4,5,COR_SPASX_EUROSTOCK50);
     for (int i = 0; i < 6; ++i)
-        PNL_MSET(*choleskyCorr,i,i,1);
+        for (int j = i; j < 6; ++j) {
+            if (i==j)
+                PNL_MSET(*choleskyCorr, i, j, 1);
+            else
+                PNL_MSET(*choleskyCorr, j, i, PNL_MGET(*choleskyCorr, i, j));
+        }
+    pnl_mat_chol(*choleskyCorr);
 }
