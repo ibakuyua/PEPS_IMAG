fd9528a45431e013eb92a42ad4cdbf1eed2039d6
Arnaud Ibakuyumcu <arnaud.ibakuyumcu@ensimag.grenoble-inp.fr>
2017-01-26 11:45:49 +0100

    [RapportBeta + GamTaux]

--- src/Pricer/RateModels/RateModelGen.hpp
+++ src/Pricer/RateModels/RateModelGen.hpp
@@ -28,22 +28,22 @@ public:
      * Abstract methods
      */
     /**
-     * Permit to get the rate for a certain maturity
+     * Permit to get the mean rate for a certain maturity
      *
-     * @param[in] maturity : the maturity
+     * @param[in] T : the maturity
      *
-     * @return the rate at the maturity asked
+     * @return the mean rate R(0,T) at the maturity T asked
      */
-    virtual double GetRate(double maturity) const = 0;
+    virtual double GetRate(double T) const = 0;
     /**
      * Permit to get the integral of the rate
      *
      * @param[in] t : lower born for the integral
-     * @param[in] maturity : upper born for the integral
+     * @param[in] T : upper born for the integral
      *
      * @return the integral between t and maturity of the rate model
      */
-    virtual double GetIntegralRate(double t, double maturity) const = 0;
+    virtual double GetIntegralRate(double t, double T) const = 0;
 };
 
 
