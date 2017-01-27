9104859c2a3b2605bc64c7879b9f25fdce66b231
Ruimy Benjamin Mac <ruimyb@gmail.com>
2017-01-27 20:13:04 +0100

    BimBim

--- src/Pricer/Tests/testHedgingMultimonde.cpp
+++ src/Pricer/Tests/testHedgingMultimonde.cpp
@@ -34,12 +34,12 @@ void computePnl(){
     setParameters(&rateModels);
 
     PnlVect *scheduleSimulation = pnl_vect_create(6);
-    LET(scheduleSimulation,0) = NB_DAYS_TO_CONSTATATION_1;
-    LET(scheduleSimulation,1) = NB_DAYS_TO_CONSTATATION_2 -  NB_DAYS_TO_CONSTATATION_1;
-    LET(scheduleSimulation,2) = NB_DAYS_TO_CONSTATATION_3 -  NB_DAYS_TO_CONSTATATION_2;
-    LET(scheduleSimulation,3) = NB_DAYS_TO_CONSTATATION_4 -  NB_DAYS_TO_CONSTATATION_3;
-    LET(scheduleSimulation,4) = NB_DAYS_TO_CONSTATATION_5 -  NB_DAYS_TO_CONSTATATION_4;
-    LET(scheduleSimulation,5) = NB_DAYS_TO_CONSTATATION_6 -  NB_DAYS_TO_CONSTATATION_5;
+    LET(scheduleSimulation,0) = NB_DAYSWRK_TO_CONSTATATION_1;
+    LET(scheduleSimulation,1) = NB_DAYSWRK_TO_CONSTATATION_2 -  NB_DAYSWRK_TO_CONSTATATION_1;
+    LET(scheduleSimulation,2) = NB_DAYSWRK_TO_CONSTATATION_3 -  NB_DAYSWRK_TO_CONSTATATION_2;
+    LET(scheduleSimulation,3) = NB_DAYSWRK_TO_CONSTATATION_4 -  NB_DAYSWRK_TO_CONSTATATION_3;
+    LET(scheduleSimulation,4) = NB_DAYSWRK_TO_CONSTATATION_5 -  NB_DAYSWRK_TO_CONSTATATION_4;
+    LET(scheduleSimulation,5) = NB_DAYSWRK_TO_CONSTATATION_6 -  NB_DAYSWRK_TO_CONSTATATION_5;
 
     //Initialisation du Modele de BlackScholes
     ModelGen *simuIndex = new BlackScholesModel(NB_ASSET, NB_ECONOMY, rateModels);
@@ -47,7 +47,7 @@ void computePnl(){
     //Initialisation du Pricer MonteCarlo
     int nbSample = 5000;
     int hedgingNb, nbTimeStep;
-    hedgingNb = 30;
+    hedgingNb = (int)Multimonde::maturity;
     PricerGen *pricer = new MonteCarloPricer(Multimonde::maturity, simuIndex, scheduleSimulation, nbSample);
     assert(pricer != NULL);
 
@@ -63,29 +63,17 @@ void computePnl(){
     cout << "\n\n";
 
 
-    //Initialisation of Past Matrix
-    double pastArray[NB_TIMEVALUE_KNOWN][NB_ASSET] =
-            {{SPOT_EUROSTOCK50,SPOT_FTSE,SPOT_P500,SPOT_HANGSENG,
-                     SPOT_NIKKEI,SPOT_SPASX200,SPOT_XFTSE,
-                     SPOT_XP500,SPOT_XHANGSENG,SPOT_XNIKKEI,SPOT_XSPASX200}}; // S(t)
-
-    PnlMat *past = pnl_mat_create(NB_TIMEVALUE_KNOWN,NB_ASSET);
-    for (int i = 0; i < NB_TIMEVALUE_KNOWN; ++i) {
-        for (int d = 0; d < NB_ASSET; ++d) {
-            PNL_MSET(past,i,d,pastArray[i][d]);
-        }
-    }
-
-
-
-
-
-            // Market initialisation
+    // Market initialisation
     cout << " --- Simulation du marché --- \n";
     PnlMat *market = pnl_mat_create_from_zero(hedgingNb + 1, NB_ASSET);
     simuIndex->SimulateMarket(Multimonde::maturity,market,hedgingNb);
     cout << " --> \033[1;34m [CHECK]\033[0m\n\n";
 
+    //PAst Initialisation
+    PnlMat *past = pnl_mat_create(1,NB_ASSET);
+    for (int i = 0; i < NB_ASSET; ++i) {
+        MLET(past,0,i) = MGET(market,0,i);
+    }
 
     // Création du portefeuille et du vecteur de PnL (PnL à chaque date)
     PnlVect *pnlAtDate = pnl_vect_create(hedgingNb + 1);
@@ -98,17 +86,21 @@ void computePnl(){
     pnl_mat_get_row(Stau_i, market, 0);
 
 
+
     double price,ic;
     double marketStep = Multimonde::maturity/(double)hedgingNb;
     cout << "Computing Price and Delta....\n";
     pricer->Price(0,past,price,ic,payOffMultimonde21);
     pricer->Delta(0,past,deltas_iMinus1,payOffMultimonde21);
-    std::cout << "Le prix en 0 est de : " << price << std::endl;
+    std::cout << "Le prix en 0 est de : " << std::endl;
     std::cout << "Les delta en 0 sont de : " << std::endl;
     pnl_vect_print(deltas_iMinus1);
     std::cout << std::endl;
+
+    pnl_mat_add_row(past, past->m, Stau_i);
     double V_iMinus1 = price  - pnl_vect_scalar_prod(deltas_iMinus1,Stau_i);
     LET(pnlAtDate, 0) = 0; // Par construction
+    std::cout << "Marché" << std::endl;
 
 
 
@@ -131,14 +123,18 @@ void computePnl(){
 
         // TEMPORARY INFORMATIONS
         // TODO : delete on time
-        std::cout << "Le pnl de la date courante est de :" << GET(pnlAtDate,i) << std::endl;
+        //std::cout << "Matrice Past" << std::endl;
+        //pnl_mat_print(past);
+        std::cout << "Le pnl de la date courante est de :" << GET(pnlAtDate,i)  << price << " à l'indice : " << i  << std::endl;
+        std::cout << "Le prix à la date courante est de :" << price << std::endl;
+        std::cout << "Les deltas sont de : " << std::endl;
+        pnl_mat_print(past);
     }
 
     // Display result
     cout << "\n-----> Pay-Off [ " << price << " ]";
     cout << "\n-----> PnL [ " << GET(pnlAtDate, hedgingNb) << " ]\n";
     cout << "\n\n\n Marché : \n\n";
-    pnl_mat_print(market);
     cout << "\n\n PnL at date : \n";
     pnl_vect_print(pnlAtDate);
 
@@ -165,6 +161,8 @@ double hedging(PricerGen *pricer, double& V_iMinus1, RateModelGen ***rateModels,
     double deltas_iStau_i = pnl_vect_scalar_prod(delta_i,Stau_i);
     double deltas_iMinus1Stau_i = pnl_vect_scalar_prod(deltas_iMinus1,Stau_i);
 
+    std::cout << "Affichage des Deltas : " << std::endl;
+    //pnl_vect_print(delta_i);
     double Vi = V_iMinus1 * capitalizationFactor - (deltas_iStau_i - deltas_iMinus1Stau_i);
 
     double ic;
