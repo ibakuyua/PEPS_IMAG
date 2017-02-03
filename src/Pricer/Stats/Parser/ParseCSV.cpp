b9d7fabd3b83a9265545c7c573bfb4a9f40322dc
Ruimy Benjamin Mac <ruimyb@gmail.com>
2017-02-03 16:15:37 +0100

    Ajout de la fonctionnalité pour parser à partir d'une date donnée

--- src/Pricer/Stats/Parser/ParseCSV.cpp
+++ src/Pricer/Stats/Parser/ParseCSV.cpp
@@ -46,6 +46,107 @@ ParseCSV::ParseCSV(string path) {
 
 }
 
+
+
+ParseCSV::ParseCSV(string path, int startYear, int startMonth, int startDay, int numberToParse){
+
+    //Creation of the pnl_mat
+    inputData = pnl_mat_new();
+    //Delimiter used to parse
+    char delimiter = ',';
+    //Declaration + Openning of the FILE
+    ifstream FILE(path,ios::in);
+
+    if(FILE){
+        char curChar = '0';
+        string line;
+        int m = numberToParse;
+        int n = 0;
+
+        //This part is used to find the number of columns
+        while(curChar != '\n'){
+            if(curChar == delimiter){
+                n++;
+            }
+            curChar = FILE.get();
+        }
+        pnl_mat_resize(inputData,m,n);
+
+        bool b = true;
+        int tmpStartDay = 0;
+        string tmpStartDayString;
+        int tmpStartMonth = 0;
+        string tmpStartMonthString;
+        int tmpStartYear = 0;
+        string tmpStartYearString;
+        char* tmp;
+
+        stringstream ss;
+        while(b){
+            getline(FILE,line);
+            tmpStartDayString = line.substr(0,2);
+            tmpStartDay = atoi(tmpStartDayString.c_str());
+
+            tmpStartMonthString = line.substr(3,2);
+            tmpStartMonth = atoi(tmpStartMonthString.c_str());
+
+            tmpStartYearString = line.substr(6,4);
+            tmpStartYear = atoi(tmpStartYearString.c_str());
+
+            if(tmpStartYear < startYear) {
+                b = false;
+            }else if(tmpStartYear == startYear){
+                if(tmpStartMonth < startMonth){
+                    b = false;
+                }else if(tmpStartMonth == startMonth){
+                    if(tmpStartDay <= startDay)
+                        b = false;
+                }
+            }
+
+        }
+
+
+        //FILE.clear();
+        //FILE.seekg(FILE.beg);
+        double tmpDouble;
+        char tmpChar;
+
+        string tmpString;
+        stringstream sss;
+        //getline(FILE,line);
+        ss << line;
+        getline(ss,tmpString,',');
+        for(int j = 0; j < n; j++){
+
+            getline(ss,tmpString,',');
+            sss << tmpString;
+            sss >> tmpDouble;
+            sss.str("");
+            sss.clear();
+            MLET(inputData,0,j) = tmpDouble;
+
+        }
+
+
+
+        for(int i = 1; i < numberToParse; i++){
+            while(FILE.get()!=delimiter){}
+            for(int j = 0; j < n; j++){
+                FILE >> tmpDouble;
+                MLET(inputData,i,j) = tmpDouble;
+                FILE >> tmpChar;
+            }
+        }
+
+    }else
+        std::cout << "Fichier CSV inexistant  !!!!!!!!!!" << endl;
+
+
+
+
+}
+
 ParseCSV::~ParseCSV() {
     pnl_mat_free(&inputData);
 }
