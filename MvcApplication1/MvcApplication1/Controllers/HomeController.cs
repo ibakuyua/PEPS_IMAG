77f04f0744269787ce4fc9429142cd594d247e39
Paul Lhuillery <Paul Lhuillery>
2017-03-16 11:04:00 +0100

    Moteur Calcul + Wrapper + Vue

--- MvcApplication1/MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wrapper;
+using MvcApplication1.Models;
+
+
 
 namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public ActionResult Index(int id = 0)
         {
             ViewBag.Message = "Pricer Multimonde.";
-
+            double prixMultimonde = 0;
+            Price priceM2021 = new Price();
+            if (id == 1)
+            {
+                prixMultimonde = priceM2021.prix;
+                //priceM2021.test();
+            }
+            ViewData.Add("prixMultimonde", prixMultimonde);
             return View();
         }
+
+        private void goBtn_Click()
+        {
+            // Retrieve the values of the parameters in the TextBoxes
+            Price priceM2021 = new Price();
+            double prixM2021 = priceM2021.prix;
+            
+        }
+
 
         public ActionResult About()
         {
---
+++ PEPS/Wrapper/Wrapper.h
@@ -0,0 +1,18 @@
+// Wrapper.h
+
+#pragma once
+#include <iostream>
+#include <string>
+using namespace System;
+
+namespace Wrapper {
+
+	public ref class Price
+	{
+	public:
+		double prix;
+		Price();
+		~Price();
+		void test();
+	};
+}
