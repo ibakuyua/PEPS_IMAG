c068e39a8f9bcdf84dcf901ed986c2a6d1d41dc6
Ruimy Benjamin Mac <ruimyb@gmail.com>
2017-03-19 17:30:02 +0100

    Ajout d'une fonction qui permet de parser à nouveau à partir du même objet parseur

--- src/Pricer/Stats/Parser/ParseCSV.cpp
+++ src/Pricer/Stats/Parser/ParseCSV.cpp
@@ -142,6 +142,108 @@ ParseCSV::ParseCSV(string path, int startYear, int startMonth, int startDay, int
 
 }
 
+
+//Ajouté par Benji test
+
+void ParseCSV::Update(string path, int startYear, int startMonth, int startDay, int numberToParse){
+
+    //Delimiter used to parse
+    char delimiter = ',';
+    //Declaration + Openning of the FILE
+    ifstream FILE(path,ios::in);
+
+    if(FILE){
+        char curChar = '0';
+        string line;
+        int m = numberToParse;
+        int n = 0;
+
+        //This part is used to find the number of columns
+        while(curChar != '\n'){
+            if(curChar == delimiter){
+                n++;
+            }
+            curChar = FILE.get();
+        }
+        pnl_mat_resize(outputData,m,n);
+
+        bool b = true;
+        int tmpStartDay = 0;
+        string tmpStartDayString;
+        int tmpStartMonth = 0;
+        string tmpStartMonthString;
+        int tmpStartYear = 0;
+        string tmpStartYearString;
+
+        while(b){
+            getline(FILE,line);
+            if (line.c_str()=="")
+                continue;
+            tmpStartDayString = line.substr(0,2);
+            tmpStartDay = atoi(tmpStartDayString.c_str());
+
+            tmpStartMonthString = line.substr(3,2);
+            tmpStartMonth = atoi(tmpStartMonthString.c_str());
+
+            tmpStartYearString = line.substr(6,4);
+            tmpStartYear = atoi(tmpStartYearString.c_str());
+
+            if(tmpStartYear < startYear) {
+                b = false;
+            }else if(tmpStartYear == startYear){
+                if(tmpStartMonth < startMonth){
+                    b = false;
+                }else if(tmpStartMonth == startMonth){
+                    if(tmpStartDay <= startDay)
+                        b = false;
+                }
+            }
+
+        }
+
+
+        double tmpDouble;
+        char tmpChar;
+
+        string tmpString;
+        stringstream ss;
+        stringstream sss;
+        ss << line;
+        getline(ss,tmpString,delimiter);
+        for(int j = 0; j < n; j++){
+
+            getline(ss,tmpString,delimiter);
+            sss << tmpString;
+            sss >> tmpDouble;
+            sss.str("");
+            sss.clear();
+            MLET(outputData,0,j) = tmpDouble;
+
+        }
+
+        for(int i = 1; i < numberToParse; i++){
+            std::cout << "Itération : " << i << std::endl;
+            // std::cout << MGET(inputData,i-1,1)
+            while(FILE.get()!=delimiter){}
+            for(int j = 0; j < n; j++){
+                FILE >> tmpDouble;
+                MLET(outputData,i,j) = tmpDouble;
+                FILE >> tmpChar;
+            }
+        }
+        FILE.close();
+
+    }else
+        std::cout << "Fichier CSV inexistant  !!!!!!!!!!" << endl;
+
+
+}
+
+//Fin de l'ajout
+
+
+
+
 ParseCSV::~ParseCSV() {
     pnl_mat_free(&outputData);
 }
