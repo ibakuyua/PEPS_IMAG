3c09ddff8a37bf2e53a0350f8b6d88f7757f4b5a
Arnaud Ibakuyumcu <arnaud.ibakuyumcu@ensimag.grenoble-inp.fr>
2017-03-29 14:04:43 +0200

    Modification pour faire des test sur les DLL export

--- src/Pricer/MultimondeFactory.cpp
+++ src/Pricer/MultimondeFactory.cpp
@@ -478,18 +478,4 @@ void getDate(int &year, int&month, int&day, double &totalDays){
 
 }
 
-void MultimondeFactory::PriceDLL(double t, int year, int month, int day, double &price, double &std, char *pathDatas) {
-    Price(t,year,month,day,price,std,pathDatas);
-}
 
-void MultimondeFactory::HedgeDLL(double t, int year, int month, int day, double *compo, double *std, char *pathDatas) {
-    Hedge(t,year,month,day,compo,std,pathDatas);
-}
-
-void MultimondeFactory::BackTestDLL(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep) {
-    BackTest(hedgingNb,MCnb,path,pathDatas,discrStep);
-}
-
-void MultimondeFactory::ForwardTestDLL(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep) {
-    ForwardTest(hedgingNb, MCnb, path, pathDatas, discrStep);
-}
