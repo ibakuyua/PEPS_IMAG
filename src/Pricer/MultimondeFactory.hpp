1bb7b6e3d113d7667e830b804e63ca97d4788311
Arnaud Ibakuyumcu <arnaud.ibakuyumcu@ensimag.grenoble-inp.fr>
2017-03-29 13:52:48 +0200

    Ajustement DLL export

--- src/Pricer/MultimondeFactory.hpp
+++ src/Pricer/MultimondeFactory.hpp
@@ -17,8 +17,10 @@ public:
      * @param[in] day  : day of the date
      * @param[out] price : price of the product
      * @param[out] std : standard deviation of monte carlo method
+     * @param[in] pathDatas : the path for the datas.csv market
      */
-	DLLEXP static void Price(double t, int year, int month, int day, double &price, double &std);
+	DLLEXP static void PriceDLL(double t, int year, int month, int day, double &price, double &std, char* pathDatas);
+	static void Price(double t, int year, int month, int day, double &price, double &std, char* pathDatas);
     /**
      * Hedge : permit to give the composition at t of the hedging portfolio
      *
@@ -31,26 +33,32 @@ public:
      *                     Must be allocated with a length of 12
      * @param[out] std : standard deviation for each delta in the previous order
      *                     Must be allocated with a length of 12
+     * @param[in] pathDatas : the path for the market datas csv file
      */
-	DLLEXP static void Hedge(double t, int year, int month, int day, double *compo, double *std);
+	DLLEXP static void HedgeDLL(double t, int year, int month, int day, double *compo, double *std, char* pathDatas);
+	static void Hedge(double t, int year, int month, int day, double *compo, double *std, char* pathDatas);
     /**
      * BackTest : permit to launch a backtest
      *
      * @param hedgingNb : Hedging number
      * @param MCnb : Number of MonteCarlo sample
-     * @param discrStep : Discretisation step for the delta method (default 0.1)
      * @param path : The path for the CSV results
+     * @param pathDatas : The path for the datas.csv
+     * @param discrStep : Discretisation step for the delta method (default 0.1)
      */
-	DLLEXP static void BackTest(int hedgingNb, int MCnb, char *path, double discrStep = 0.1);
+	DLLEXP static void BackTestDLL(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep = 0.1);
+	static void BackTest(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep = 0.1);
     /**
      * ForwardTest : permit to laucnh a forward test
      *
      * @param hedgingNb : Hedging number
      * @param MCnb : Number of MonteCarlo sample
-     * @param discrStep : Discretisation step for the delta method (default 0.1)
      * @param path : The path for the CSV results
+     * @param pathDatas : The path for the datas.csv
+     * @param discrStep : Discretisation step for the delta method (default 0.1)
      */
-	DLLEXP static void ForwardTest(int hedgingNb, int MCnb, char *path, double discrStep = 0.1);
+	DLLEXP static void ForwardTestDLL(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep = 0.1);
+	static void ForwardTest(int hedgingNb, int MCnb, char *path, char *pathDatas, double discrStep = 0.1);
 };
 
 
---
+++ src/Pricer/Tests/HedgingTest/testHedgingMultimondeBT.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <iostream>
+#include <assert.h>
+#include <fstream>
+#include "../../MultimondeFactory.hpp"
+
+
+using namespace std;
+void computePnl(int hedgingNb);
+
+int main(int argc, char** argv){
+    int hedgingNb(5);
+    if (argc > 1)
+        hedgingNb = atoi(argv[1]);
+    computePnl(hedgingNb);
+    return EXIT_SUCCESS;
+}
+
+void computePnl(int hedgingNb) {
+
+    cout << "\n\n###### TEST OF HEDGING MULTIMONDE (BACKTEST MARKET) ######\n\n";
+    int monteCarloNb = 50000;
+    double discrStep = 0.1;
+    char *path = strdup(string("../data/backTest.csv").c_str());
+    char *pathDatas = strdup(string("../data/dataPEPS.csv").c_str());
+    MultimondeFactory::BackTest(hedgingNb,monteCarloNb,path, pathDatas, discrStep);
+    // Delete
+    free(path);
+    free(pathDatas);
+}
+
